300840a01c0f01f629cefb8f1c51c635eecd4d6e
Pzf874 <58837649+Pzf874@users.noreply.github.com>
2023-10-16 13:14:57 +0800

    Add Qihu360F5Pro config dir
    Qihu360F5Pro board config files dir

---
+++ trunk/configs/boards/360F5Pro/board.h
@@ -0,0 +1,31 @@
+/* 360F5Pro */
+
+#define BOARD_PID		"360F5Pro"
+#define BOARD_NAME		"360F5Pro"
+#define BOARD_DESC		"360 Firewall5 Pro Wireless Router"
+#define BOARD_VENDOR_NAME	"Beijing Qihu360 Technology Co., Ltd."
+#define BOARD_VENDOR_URL	"https://life.360.cn/"
+#define BOARD_MODEL_URL		"http://luyou.360.cn/home/specification/f5p"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	18
+#undef  BOARD_GPIO_BTN_WPS
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_POWER	8	/* 6: red, 10: yellow, 8: blue */
+#undef  BOARD_GPIO_LED_LAN
+#define  BOARD_GPIO_LED_WAN	12
+#undef  BOARD_GPIO_LED_USB	
+#undef  BOARD_GPIO_LED_ROUTER
+#undef BOARD_GPIO_PWR_USB_ON	
+#undef BOARD_GPIO_PWR_USB	
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	4
+#define BOARD_NUM_ANT_5G_RX	4
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	4
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+
