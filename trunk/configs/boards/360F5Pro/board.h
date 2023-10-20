f3beda6098b4f3f4b951b951df1a33875c01396e
Pzf874 <58837649+Pzf874@users.noreply.github.com>
2023-10-20 08:56:52 +0800

    Add files via upload

--- trunk/configs/boards/360F5Pro/board.h
+++ trunk/configs/boards/360F5Pro/board.h
@@ -6,19 +6,20 @@
 #define BOARD_VENDOR_NAME	"Beijing Qihu360 Technology Co., Ltd."
 #define BOARD_VENDOR_URL	"https://life.360.cn/"
 #define BOARD_MODEL_URL		"http://luyou.360.cn/home/specification/f5p"
-#define BOARD_BOOT_TIME		20
+#define BOARD_BOOT_TIME		30
 #define BOARD_FLASH_TIME	120
 #define BOARD_GPIO_BTN_RESET	18
 #undef  BOARD_GPIO_BTN_WPS
 #undef  BOARD_GPIO_LED_ALL
 #undef  BOARD_GPIO_LED_WIFI
+#undef  BOARD_GPIO_LED_SW2G
+#undef  BOARD_GPIO_LED_SW5G
 #define BOARD_GPIO_LED_POWER	8	/* 8: blue */
 #undef  BOARD_GPIO_LED_LAN
-#undef  BOARD_GPIO_LED_WAN
-#undef  BOARD_GPIO_LED_USB	
-#undef  BOARD_GPIO_LED_ROUTER
-#undef  BOARD_GPIO_PWR_USB_ON	
-#undef  BOARD_GPIO_PWR_USB	
+#undef BOARD_GPIO_LED_WAN
+#undef BOARD_GPIO_LED_USB
+#undef BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX	4
 #define BOARD_NUM_ANT_5G_RX	4
@@ -28,4 +29,6 @@
 #define BOARD_NUM_ETH_EPHY	5
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	0
+#define BOARD_USB_PORT_SWAP	0
 
